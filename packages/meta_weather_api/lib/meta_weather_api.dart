36e5507324a18d6ace8f4e753ad3c4ceb24d025f
Jaroslav Novotný <novotnyj912@gmail.com>
2022-02-19 13:14:48 +0100

    Added weather API package

---
+++ packages/meta_weather_api/lib/meta_weather_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library meta_weather_api;
+
+export 'src/meta_weather_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.

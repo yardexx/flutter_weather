5007b835ceec2d72c8bb28b859f9d7ac1d21d02a
Jaroslav Novotný <novotnyj912@gmail.com>
2022-02-19 14:07:16 +0100

    Added serialization

--- packages/meta_weather_api/lib/src/models/models.dart
+++ packages/meta_weather_api/lib/src/models/models.dart
@@ -1,2 +1,2 @@
-export 'locations.dart';
+export 'location.dart';
 export 'weather.dart';
--- packages/meta_weather_api/lib/src/models/weather.dart
+++ packages/meta_weather_api/lib/src/models/weather.dart
@@ -1,44 +1,57 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'weather.g.dart';
+
 enum WeatherState {
-  snow,
-  sleet,
-  hail,
-  thunderstorm,
-  heavyRain,
-  lightRain,
-  showers,
-  heavyCloud,
-  lightCloud,
-  clear,
-  unknown
+@JsonValue('sn')
+snow,
+@JsonValue('sl')
+sleet,
+@JsonValue('h')
+hail,
+@JsonValue('t')
+thunderstorm,
+@JsonValue('hr')
+heavyRain,
+@JsonValue('lr')
+lightRain,
+@JsonValue('s')
+showers,
+@JsonValue('hc')
+heavyCloud,
+@JsonValue('lc')
+lightCloud,
+@JsonValue('c')
+clear,
+unknown
+}
+
+extension WeatherStateX on WeatherState {
+  String? get abbr => _$WeatherStateEnumMap[this];
 }
 
 enum WindDirectionCompass {
-  north,
-  northEast,
-  east,
-  south,
-  southWest,
-  west,
-  northWest,
-  unknown
+@JsonValue('N')
+north,
+@JsonValue('NE')
+northEast,
+@JsonValue('E')
+east,
+@JsonValue('SE')
+southEast,
+@JsonValue('S')
+south,
+@JsonValue('SW')
+southWest,
+@JsonValue('W')
+west,
+@JsonValue('NW')
+northWest,
+unknown,
 }
 
+@JsonSerializable()
 class Weather {
-  final int id;
-  final String weatherStateName;
-  final WeatherState weatherStateAbbr;
-  final WindDirectionCompass windDirectionCompass;
-  final DateTime created;
-  final DateTime applicableDate;
-  final double minTemp;
-  final double maxTemp;
-  final double windSpeed;
-  final double windDirection;
-  final double airPressure;
-  final int humidity;
-  final double visibility;
-  final int predictability;
-
   const Weather({
     required this.id,
     required this.weatherStateName,
@@ -48,6 +61,7 @@ class Weather {
     required this.applicableDate,
     required this.minTemp,
     required this.maxTemp,
+    required this.theTemp,
     required this.windSpeed,
     required this.windDirection,
     required this.airPressure,
@@ -55,4 +69,25 @@ class Weather {
     required this.visibility,
     required this.predictability,
   });
+
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
+
+  final int id;
+  final String weatherStateName;
+  @JsonKey(unknownEnumValue: WeatherState.unknown)
+  final WeatherState weatherStateAbbr;
+  @JsonKey(unknownEnumValue: WindDirectionCompass.unknown)
+  final WindDirectionCompass windDirectionCompass;
+  final DateTime created;
+  final DateTime applicableDate;
+  final double minTemp;
+  final double maxTemp;
+  final double theTemp;
+  final double windSpeed;
+  final double windDirection;
+  final double airPressure;
+  final int humidity;
+  final double visibility;
+  final int predictability;
 }
